869a78e6e2a2de6ee1c63bb63003345fa8247b7a
Ihar Saroka <Ihar_Saroka@epam.com>
2018-08-15 14:20:26 +0300

    master: added function logFirstAvailable

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,48 @@
-function getAllBooks(): any {
-  const books: any[] = [
-    {
-      author: "Evan Burchard",
-      available: true,
-      title: "Refactoring JavaScript",
-    },
-    {
-      author: "Liang Yuxian Eugene",
-      available: false,
-      title: "JavaScript Testing",
-    },
-    {
-      author: "Lea Verou",
-      available: true,
-      title: "CSS Secrets",
-    },
-    {
-      author: "Andrea Chialerri",
-      available: true,
-      title: "Mastering JavaScript Object-Oriented Programming",
-    },
-  ];
-  return books;
+function getAllBooks(): any[] {
+    const books: any[] = [
+        {
+            author: "Evan Burchard",
+            available: true,
+            title: "Refactoring JavaScript",
+        },
+        {
+            author: "Liang Yuxian Eugene",
+            available: false,
+            title: "JavaScript Testing",
+        },
+        {
+            author: "Lea Verou",
+            available: true,
+            title: "CSS Secrets",
+        },
+        {
+            author: "Andrea Chialerri",
+            available: true,
+            title: "Mastering JavaScript Object-Oriented Programming",
+        },
+    ];
+    return books;
 }
 
+function logFirstAvailable(books: any[]): void {
+    const amountOfBooks: number = books.length;
+    let firsrtAvailable: string = "";
+
+    for (const book of books) {
+        if (book.available) {
+            firsrtAvailable = book.title;
+            break;
+        }
+    }
+
+  /* tslint:disable:no-console */
+    console.log(`Amount of books: ${amountOfBooks}`);
+    console.log(`First available book: ${firsrtAvailable}`);
+  /* tslint:enable:no-console */
+}
+
+logFirstAvailable(getAllBooks());
+
 document.getElementById("typescript-app").innerText = JSON.stringify(
-  getAllBooks(),
+    getAllBooks(),
 );
