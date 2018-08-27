ab4d690c110f6dae73151f164c29d23de3ace180
Ihar Saroka <Ihar_Saroka@epam.com>
2018-08-27 12:07:03 +0300

    extending-interface: created variables of types IAuthor and ILibrarian

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,21 @@ interface ILibrarian extends IPerson {
     department: string;
     assistCustomer: (custName: string) => void;
 }
+
+const favoriteAuthor: IAuthor = {
+    email: 'author@google.com',
+    name: 'author name',
+    numBooksPublished: 12,
+};
+
+const favoriteLibrarian: ILibrarian = {
+    assistCustomer: (custName: string) => {
+        console.log(`Can i help you ${custName}?`);
+    },
+    department: 'Science',
+    email: 'author@google.com',
+    name: 'author name',
+};
 // ************************************************
 
 // Task 8. Defining interface for Function types
