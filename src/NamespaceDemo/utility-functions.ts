dee42666f91566cf68543ed9367fd32974c21ac5
Ihar Saroka <Ihar_Saroka@epam.com>
2018-08-31 13:30:59 +0300

    using-namespaces: created function calculateLateFee inside namespace Fees

--- src/NamespaceDemo/utility-functions.ts
+++ src/NamespaceDemo/utility-functions.ts
@@ -1,6 +1,10 @@
 /* tslint:disable:no-namespace*/
 namespace Utility {
-    export namespace Fees {  }
+    export namespace Fees {
+        export function calculateLateFee(daysLate: number): number {
+            return daysLate * 0.25;
+        }
+    }
 }
 
 /* tslint:enable:no-namespace*/
