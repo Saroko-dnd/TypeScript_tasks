2fe1945a295462b07110147b8003c37fa79bdc26
Ihar Saroka <Ihar_Saroka@epam.com>
2018-08-31 13:38:30 +0300

    using-namespaces: added function maxBooksAllowed to namespace Utility

--- src/NamespaceDemo/utility-functions.ts
+++ src/NamespaceDemo/utility-functions.ts
@@ -1,5 +1,9 @@
 /* tslint:disable:no-namespace*/
 namespace Utility {
+    export function maxBooksAllowed(age: number): number {
+        return age < 12 ? 3 : 10;
+    }
+
     export namespace Fees {
         export function calculateLateFee(daysLate: number): number {
             return daysLate * 0.25;
