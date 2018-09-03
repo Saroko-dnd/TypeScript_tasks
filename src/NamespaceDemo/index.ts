4050ae0ecafebaa19ebd4e5c494ff14877507b38
Ihar Saroka <Ihar_Saroka@epam.com>
2018-09-03 10:48:59 +0300

    using-namespaces: added alias for Utility.Fees using import keyword

--- src/NamespaceDemo/index.ts
+++ src/NamespaceDemo/index.ts
@@ -1,3 +1,11 @@
 /* tslint:disable:no-reference*/
-/// <reference path="utility-functions.ts" />
+/// <reference path="./utility-functions.ts" />
 /* tslint:enable:no-reference*/
+
+import Fees = Utility.Fees;
+
+console.log(`Task 14: Using Namespaces`);
+console.log(
+    `Max allowed number of books fo 15 years old:
+    ${Utility.maxBooksAllowed(15)}`,
+);
