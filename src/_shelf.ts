1689be211027ffe243e6b8c0419d2106778a9c6c
Ihar Saroka <Ihar_Saroka@epam.com>
2018-09-04 16:15:52 +0300

    generic-constraints: added several new methods as well as constraint for generic type in class Shelf

--- src/_shelf.ts
+++ src/_shelf.ts
@@ -1,4 +1,8 @@
-export default class Shelf<T> {
+interface IShelfItem {
+    title: string;
+}
+
+export default class Shelf<T extends IShelfItem> {
     private items: T[] = [];
 
     public add(item: T): void {
@@ -7,5 +11,13 @@ export default class Shelf<T> {
 
     public getFirst(): T {
         return this.items[0];
+    }
+
+    public find(title: string): T {
+        return this.items.find((item) => item.title === title);
+    }
+
+    public printTitles(): void {
+        this.items.forEach((item) => console.log(item.title));
     }
 }
